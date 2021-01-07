f0e6987a2acf3b4a109a1563ee50e06fd01e8558
Dominique Vernier <dvernier@redhat.com>
2021-01-07 09:32:21 -0500

    add -v

--- vbh/build-harness-extensions/modules/component/bin/component/coverage-entrypoint-func.sh
+++ vbh/build-harness-extensions/modules/component/bin/component/coverage-entrypoint-func.sh
@@ -7,8 +7,6 @@
 # The parameters are stored in a secret aws-s3-coverage in the open-cluster-management namespace.
 # It uses the POD internal hostname to connect with kube and so must run in a POD.
 
-set -x
-
 function getAWSParams() {
     APISERVER=https://kubernetes.default.svc
     SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
