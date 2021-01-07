54806dca9465b1d338f13c4b8185160c16bebe0b
Dominique Vernier <dvernier@redhat.com>
2021-01-07 17:38:20 -0500

    Fix POD_NAME

--- build/run-functional-tests.sh
+++ build/run-functional-tests.sh
@@ -114,6 +114,7 @@ for dir in overlays/test/* ; do
   # patch image
   echo "Wait rollout"
   kubectl rollout status -n open-cluster-management deployment clusterlifecycle-state-metrics --timeout=90s
+  POD_NAME=`kubectl get pods -n open-cluster-management | grep clusterlifecycle-state-metrics | cut -d ' ' -f1`
   
   # exit 1
 
@@ -122,7 +123,6 @@ for dir in overlays/test/* ; do
   make functional-test
   if [ $? != 0 ]; then
     ERR=$?
-    POD_NAME=`kubectl get pods -n open-cluster-management | grep clusterlifecycle-state-metrics | cut -d ' ' -f1`
     kubectl logs $POD_NAME -n open-cluster-management
     exit $ERR
   fi
