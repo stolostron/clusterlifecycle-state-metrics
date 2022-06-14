e69613f426113259a990805f8aacd776cd5efd5f
xuezhaojun <zxue@redhat.com>
2022-06-14 16:36:40 +0800

    lint is not used in test
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -12,9 +12,6 @@ KubeBuilderVersion="2.3.0"
 
 if ! which patter > /dev/null; then      echo "Installing patter ..."; go get -u github.com/apg/patter; fi
 if ! which gocovmerge > /dev/null; then  echo "Installing gocovmerge..."; go get -u github.com/wadey/gocovmerge; fi
-if ! which golangci-lint > /dev/null; then
-   curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.46.2
-fi
 if ! which go-bindata > /dev/null; then
 	echo "Installing go-bindata..."
 	cd $(mktemp -d) && GOSUMDB=off go get -u github.com/go-bindata/go-bindata/...
